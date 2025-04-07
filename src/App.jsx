c73bb24e3ed04d44b86d81e90736ac275ad3547d
Nikita Horkavchuk <horkavchuk.nik@gmail.com>
2025-04-08 01:04:59 +0300

    upd App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useEffect } from "react"
 import "./App.css"
 
 import { Header } from "./components/Header"
 import { TodoForm } from "./components/TodoForm"
 import { TodoList } from "./components/TodoList"
 
-import { useTodosContext } from "./hooks/useTodosContext"
-
 function App() {
-    const { todos } = useTodosContext()
-    useEffect(() => {
-        console.log(todos)
-    }, [todos])
     return (
         <div className="App">
             <Header />
